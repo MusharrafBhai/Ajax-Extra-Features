6a0823c80704f46883d955ff9779d4ccb8595c94
RoyalDeep <110165648+RoyalDeep@users.noreply.github.com>
2022-07-28 17:20:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,12 +42,12 @@ async def start(client, message):
             InlineKeyboardButton('➕Add Me To Your Group➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍Search Inline', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('▶️Subscribe', url='https://t.me/WOMBACKUP')
+            InlineKeyboardButton('😎Owner', url='https://t.me/WOMBACKUP')
             ],[      
             InlineKeyboardButton('ℹ️Help', callback_data='help'),
             InlineKeyboardButton('😊About', callback_data='about')
             ],[
-            InlineKeyboardButton('🛎Youtube🛎', url='https://www.youtube.com/channel/UCBTsuBKjrtuEQbC9RtksLzw')
+            InlineKeyboardButton('🛎 Youtube 🛎', url='https://www.youtube.com/channel/UCBTsuBKjrtuEQbC9RtksLzw')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
